e97335658b6d20f7e2e36977dc1401058cd36746
Thiago Thalisson <atomic-feast@hotmail.com>
2023-06-16 20:14:22 -0300

    changed somethings in the menu

--- src/slug.ts
+++
@@ -1,8 +0,0 @@
-export const slugify = (string: string) =>
-  String(string)
-    .normalize("NFD") // removes accents
-    .replace(/[\u0300-\u036f]/g, "") // needed because im using normalize
-    .toLowerCase()
-    .replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "") // removes special characters
-    .split(" ")
-    .join("-")
